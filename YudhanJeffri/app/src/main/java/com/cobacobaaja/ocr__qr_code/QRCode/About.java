b7085e1de882d53d68b9e3da2b88355bd9fda9e0
YudhanJeffri <yudhanjeffri168@gmail.com>
2020-02-09 11:36:52 +0700

    OCR and QR Code Scanner

---
+++ YudhanJeffri/app/src/main/java/com/cobacobaaja/ocr__qr_code/QRCode/About.java
@@ -0,0 +1,16 @@
+package com.cobacobaaja.ocr__qr_code.QRCode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cobacobaaja.ocr__qr_code.R;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
